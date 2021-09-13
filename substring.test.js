ec10138ae72791e798deb6b78e36acadeb7b21d0
Jeevan D <jeevu.art@gmail.com>
2021-09-13 15:26:42 +0700

    Add more tests to validate the result

--- substring.test.js
+++ substring.test.js
@@ -10,3 +10,14 @@ assert.equal(result, 'kfxabc')
 result = getLongestNonRepeatingSubstring('pxxkfxafcdeabnx');
 assert.equal(result, 'fcdeabnx')
 
+result = getLongestNonRepeatingSubstring('qweqweqweqeqweqgyugbytrfrt');
+assert.equal(result, 'ugbytrf')
+
+result = getLongestNonRepeatingSubstring('manmannmannnmannmannmann');
+assert.equal(result, 'anm')
+
+result = getLongestNonRepeatingSubstring('trycatchfinally');
+assert.equal(result, 'tchfinal')
+
+result = getLongestNonRepeatingSubstring('qwertyqwertyuqwertyui');
+assert.equal(result, 'qwertyui')
