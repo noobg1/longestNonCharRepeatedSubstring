aa24e1c72a69657ccbba7f48c226ad421935a239
Jeevan D <jeevu.art@gmail.com>
2021-09-13 15:20:12 +0700

    Add comments

---
+++ README.md
@@ -0,0 +1,17 @@
+### Longest substring in a string with non-repeating chars
+
+
+### Tests
+```
+node substring.test.js
+```
+
+### Complexity
+
+```
+mem: O(N * M ) // for storing occurrences details and array of substrings
+time: O(N * M )
+
+N = length on text
+M = length of largest substring
+```
--- substring.js
+++ substring.js
@@ -1,23 +1,26 @@
 const getLongestNonRepeatingSubstring = (text) => {
+  // pointers to track the largest non repeating chars
   let leftNeedle = 0
   let rightNeedle = 1
   let currSubstring = ''
-  let currSubstringMap = {}
-  let substringList = []
+  let currSubstringCharMap = {} // collect occurrences of a char in currSubstring
+  let substringList = [] // collect all substrings
   while (leftNeedle < text.length && rightNeedle < text.length) {
-    if (!Boolean(currSubstringMap[text[rightNeedle]]) || currSubstringMap[text[rightNeedle]] < 1) {
+    if (!Boolean(currSubstringCharMap[text[rightNeedle]]) || currSubstringCharMap[text[rightNeedle]] < 1) {
       currSubstring = currSubstring + text[rightNeedle]
-      currSubstringMap[text[rightNeedle]] = 1
+      currSubstringCharMap[text[rightNeedle]] = 1
       rightNeedle++;
     } else {
+      // reset needles by one character after getting a duplicated character
       leftNeedle++
       rightNeedle = leftNeedle + 1
       currSubstring = ''
-      currSubstringMap = {}
+      currSubstringCharMap = {}
     }
     substringList.push(currSubstring);
   }
 
+  // Find the max substring from the list
   let maxLength = 0;
   let maxLengthIndex = 0;
   substringList.forEach((substring, index) => {
